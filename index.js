e1b4d5186cd1577ebc499ef8689cffa8574c220d
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-06-02 13:34:02 -0500

    install dotenv

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const apiKey = process.env.KEY
 
 const displaySection = document.querySelector('.display-section')
 
-const data = null
 
-const data = null;
+function getBooks() {
+  const data = null
+  const xhr = new XMLHttpRequest()
+  xhr.withCredentials = true
 
-const xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+  xhr.addEventListener("readystatechange", function() {
+    if (this.readyState === this.DONE) {
+      console.log(this.responseText)
+    }
+  })
 
-xhr.addEventListener("readystatechange", function () {
-	if (this.readyState === this.DONE) {
-		console.log(this.responseText);
-	}
-});
+  xhr.open('Get', 'https://ajith-holy-bible.p.rapidapi.com/GetBooks')
+  xhr.setRequestHeader('X-RapidAPI-Host', 'ajith-holy-bible.p.rapidapi.com')
+  xhr.setRequestHeader('X-RapidAPI-Key', `${apiKey}`)
 
-xhr.open("GET", "https://ajith-holy-bible.p.rapidapi.com/GetBooks");
-xhr.setRequestHeader("X-RapidAPI-Host", "ajith-holy-bible.p.rapidapi.com");
-xhr.setRequestHeader("X-RapidAPI-Key", "3930eb67bdmshbd8e623113ff02fp19c25bjsn71dfb42a64cd");
-
-xhr.send(data);
+  xhr.send(data)
+}
