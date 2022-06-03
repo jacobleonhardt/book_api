254d4d72a611b7aa7107ef65c5a664df83bafd14
Jacob Leonhardt <jacob.leonhardt649@gmail.com>
2022-06-03 16:25:42 -0500

    update privacy to use config instead of env

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const dotenv = require('dotenv')
-dotenv.config()
-
-const apiKey = process.env.KEY
-
+const key = apiInfo.SECRET_KEY
 
 function getBooks() {
+
   const displaySection = document.querySelector('.display-section')
   const data = null
   const xhr = new XMLHttpRequest()
@@ -18,7 +15,7 @@ function getBooks() {
 
   xhr.open('Get', 'https://ajith-holy-bible.p.rapidapi.com/GetBooks')
   xhr.setRequestHeader('X-RapidAPI-Host', 'ajith-holy-bible.p.rapidapi.com')
-  xhr.setRequestHeader('X-RapidAPI-Key', `${apiKey}`)
+  xhr.setRequestHeader('X-RapidAPI-Key', `${key}`)
 
   xhr.send(data)
 }
